a21341934d1296ca551e597c50f42ae71ca300a6
Hussein Parpia <hparpia8@gmail.com>
2019-02-14 12:56:20 -0330

    Figuring out Mongo client

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -12,33 +12,54 @@ DatabaseManager = function () {
 	this.url = "mongodb://localhost:27017/apoRun";
 	this.collections = ['user', 'asset'];
 
+	// Create database;
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-
-		db.listCollections().toArray(function(err, collInfos) {
-			if (err) throw err;
-
-			for (var i in this.collections) {
-
-				var collectionExists = collInfos.find(function (col) {
-					return col.name == this.collections[i];
-				});
-
-				if (collectionExists == null) {
-					// Create collection if it doesn't already exists.
-					db.createCollection(this.collections[i], function(err, res) {
-						if (err) {
-							console.log("Nothing here", err);
-							throw err;
-						}
-						console.log(this.collections[i]," - Collection created!");
-					});
-				}
-			}
-			
-		});
+		
+		// Checks which collection needs to be created;
+		console.log("Here0");
+		var toCreate = this.collectionsToCreate(db);
+		console.log("To be created: ", toCreate);
+		for (var i in toCreate) {
+			console.log("Here2");
+			db.createCollection(toCreate[i], function(err, res) {
+				if (err) throw err;
+				console.log(toCreate[i]," - Collection created!");
+			});
+		}
+		
+		// Close database after write
 		db.close();
+		
 	});
+
+	this.collectionsToCreate = function(db) {
+
+			var toCreate = new Array();
+			// Check if the collections already exists.
+			db.listCollections().toArray(function(err, collInfos) {
+
+				console.log("Here1");
+				for (var i in this.collections) {
+
+					var collectionExists = collInfos.find(function (col) {
+						return col.name == this.collections[i];
+					});
+
+					if (collectionExists == null) {
+						// Create collection if it doesn't already exists.
+						toCreate.push(this.collections[i]);
+						console.log(this.collections[i]);
+					}
+				}
+				
+			});
+			console.log("To create: ", toCreate);
+			return toCreate;
+		}
+
+	
+	
 }
 
 DatabaseManager();
