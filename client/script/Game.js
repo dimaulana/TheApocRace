d721292e8b761a94778734449c0f9506f2351157
hparpia8 <hparpia8@gmail.com>
2019-03-18 18:19:32 -0230

    Fixed collisions

--- client/script/Game.js
+++ client/script/Game.js
@@ -14,46 +14,40 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 var display = document.querySelector('#game').getContext("2d");
 
+ctx.font= "40px arcade";
+
+// Dimensions of the player images;
+const SPRITE_SIZE = 40;
 
 var player, sprite_sheet, backgroundSound, level;
 var obstacles = [];
 var paused = true; // When the game has not started, paused is true in order to stop the updates;
 
-var topScore=0; // Later to come from the database taken compared to other players
-
-ctx.font= "40px arcade";
-
-// Dimensions of the player images;
-const SPRITE_SIZE = 40;
+var topScore = 0; // Later to come from the database taken compared to other players
 
 // To keep track of the player sprite;
 sprite_sheet = {
 	frame_sets:[[0], [1], [2, 3, 4], [5, 6, 7]] // standing, running right, running left;
 };
 
-// TODO: To get from asset manager;
-var Img = {};
-
-testCollisionRectRect = function(rect1,rect2){
-	return rect1.x <= rect2.x + rect2.width
-	&& rect2.x <= rect1.x +rect1.width
-	&& rect1.y <= rect2.y + rect2.height
-	&& rect2.y <= rect1.y + rect1.height;
-}
-
-function Tile(src, locationX) {
+function Tile(imageSource, locationX) {
+	this.tileImage = new Image();
+	this.tileImage.src = imageSource;
 	this.width = 30;
 	this.height = 41;
 	this.tileImage = new Image();
 	this.tileImage.src = imageSource;
 	this.x = locationX;
 	this.y = canvas.height - this.height;
+	this.prevX = 0;
+	this.prevY = this.y;
 
 	this.draw = function() {
 		ctx.drawImage(this.tileImage,this.x, this.y);
 	}
 
 	this.update = function() {
+		this.prevX = this.x;
 		if (player.x === canvas.width/2)
 			this.x -= player.speedX;
 	}
@@ -68,6 +62,8 @@ Player = function(param) {
 		speedX: param.speed.x,
 		speedY: param.speed.y,
 		speedMax: param.speedMax,
+		scaleX: param.scale.x,
+		scaleY: param.scale.y,
 		hp: param.hp,
 		score: param.score,
 		lives: param.lives,
@@ -80,6 +76,7 @@ Player = function(param) {
 		left: false,
 		up:  false,
 		down: false,
+		jump: false,
 		state: "stand",
 		animation: new Animation(),
 		image: new Image(),
@@ -97,9 +94,9 @@ Player = function(param) {
 		// TODO: Collision should do this with the tiles;
 		self.y -= self.gravity;
 
-		if (self.x === canvas.width/2) {
-			return;
-		}
+		// if (self.x === canvas.width/2) {
+		// 	return;
+		// }
 
 		if (self.x > canvas.width/2) {
 			self.x = canvas.width/2;
@@ -114,20 +111,24 @@ Player = function(param) {
 			self.speedX = self.speedMax;
 			self.animation.change(sprite_sheet.frame_sets[2], delay);
 			self.state = "run";
+			self.scaleX = 1.0;
 		}
-		else if (self.left && ((self.x - self.speedMax) > 0)) {
+		else if (self.left) {
 			self.speedX = -self.speedMax;
 			self.animation.change(sprite_sheet.frame_sets[3], delay);
 			self.state = "run";
+			self.scaleX = -1.0;
 		}
 		else {
+
 			self.speedX = 0;
-			self.animation.change(sprite_sheet.frame_sets[0], delay);
+			// If scale is -1 choose the opposite facing sprite;
+			self.animation.change(sprite_sheet.frame_sets[(self.stateX == -1.0) ? 1 : 0], delay);
 			self.state = "stand";
 		}
 
-		if(self.up && self.state != "jump") {
-			self.up = false;
+		if(self.jump && self.state != "jump") {
+			// self.up = false;
 			// TODO: Get a better jump speed;
 			self.speedY = -self.speedMax*6;
 			self.state = "jump";
@@ -137,43 +138,6 @@ Player = function(param) {
 			self.state = "stand";
 		}
 	}
-
-	self.testCollisions = function () {
-
-		obstacles.forEach(function(entity) {
-
-			var playerCX = self.x + self.width * 0.5;
-			var entityCX = entity.x + entity.width * 0.5;
-
-			var playerCY = self.y + self.height * 0.5;
-			var entityCY = entity.y + entity.height * 0.5;
-
-			var dx =  playerCX - entityCX; // x difference between centers
-			var dy = playerCY - entityCY; // y difference between centers
-			var aw = (self.width + entity.width) * 0.5;// average width
-			var ah = (self.height + entity.height) * 0.5;// average height
-
-			// If either distance is greater than the average dimension there is no collision. //
-			if (Math.abs(dx) > aw || Math.abs(dy) > ah) return false;
-
-        	// To determine which region of this rectangle the rect's center
-        	// point is in, we have to account for the scale of the this rectangle.
-        	// To do that, we divide dx and dy by it's width and height respectively. //
-        	if (Math.abs(dx / entity.width) > Math.abs(dy / entity.height)) {
-        		if (dx < 0) self.x = entity.x - self.width;// left
-        		else self.x = entity.x + self.width; // right
-
-        	} else {
-
-	        	if (dy < 0) self.y = entity.y - self.height; // top
-        		else self.y = entity.y + self.height; // bottom
-        	}
-
-        	return true;
-		});
-	}
-
-
 	// Draw the player based on the current frame;
 	self.draw = function() {
 		ctx.drawImage(self.image, self.animation.frame * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE*2,
@@ -182,6 +146,110 @@ Player = function(param) {
 	}
 
 	return self;
+}
+
+function getOverlap(a, b) {
+	var delta = { x: Math.abs(a.x - b.x),
+				  y: Math.abs(a.y - b.y)
+				}
+
+	var halfSizeA = { x: a.width / 2, y: a.height / 2 };
+	var halfSizeB = { x: b.width / 2, y: b.height / 2 };
+
+	var overlapX = halfSizeA.x + halfSizeB.x - delta.x;
+	var overlapY = halfSizeA.y + halfSizeB.y - delta.y;
+
+	return {x: overlapX, y: overlapY};
+}
+
+function getPrevOverlap(a, b) {
+	var delta = { x: Math.abs(a.prevX - b.prevX),
+				  y: Math.abs(a.prevY - b.prevY)
+				}
+
+	var halfSizeA = { x: a.width / 2, y: a.height / 2 };
+	var halfSizeB = { x: b.width / 2, y: b.height / 2 };
+
+	var overlapX = halfSizeA.x + halfSizeB.x - delta.x;
+	var overlapY = halfSizeA.y + halfSizeB.y - delta.y;
+
+	return {x: overlapX, y: overlapY};
+}
+
+var testCollisions = function () {
+
+/* DEPRACATED
+	obstacles.forEach(function(entity) {
+
+		var playerCX = player.x + player.width * 0.5;
+		var entityCX = entity.x + entity.width * 0.5;
+
+		var playerCY = player.y + player.height * 0.5;
+		var entityCY = entity.y + entity.height * 0.5;
+
+		var dx =  playerCX - entityCX; // x difference between centers
+		var dy = playerCY - entityCY; // y difference between centers
+		var aw = (player.width + entity.width) * 0.5;// average width
+		var ah = (player.height + entity.height) * 0.5;// average height
+
+		// If either distance is greater than the average dimension there is no collision. //
+		if (Math.abs(dx) > aw || Math.abs(dy) > ah) return false;
+
+    	// To determine which region of this rectangle the rect's center
+    	// point is in, we have to account for the scale of the this rectangle.
+    	// To do that, we divide dx and dy by it's width and height respectively. //
+    	if (Math.abs(dx / entity.width) > Math.abs(dy / entity.height)) {
+    		if (dx < 0) player.x = entity.x - player.width;// left
+    		else player.x = entity.x + player.width; // right
+
+    	} else {
+
+        	if (dy < 0) self.y = entity.y - self.height; // top
+    		else self.y = entity.y + self.height; // bottom
+    	}
+
+    	return true;
+    });
+*/
+
+
+ 	obstacles.forEach(function(entity) {
+
+  		var currentOverlap = getOverlap(player, entity);
+  		var prevOverlap = getPrevOverlap(player, entity);
+
+  		if (currentOverlap.x > 0 && currentOverlap.y > 0) {
+
+  			if (prevOverlap.x > 0) {
+  				// Collision from top or bottom;
+  				if ((player.y - player.prevY) > 0) {
+  					// Collision came from top;
+  					player.state = "stand";
+  					player.speedY = 0;
+  					player.y -= currentOverlap.y;
+  				}
+  				else if (((player.y - player.prevY) > 0)) {
+  					// Collision came from bottom of tile;
+  					player.speedY = 0;
+  					player.y += currentOverlap.y;
+  				}
+  			}
+
+  			if (prevOverlap.y > 0) {
+  				// Collision from right or left;
+  				if ((player.x - player.prevX) > 0) {
+  					// Collision from right;
+  					player.speedX = 0;
+  					player.x += currentOverlap.x;
+  				}
+  				else if ((player.x - player.prevX) < 0) {
+  					// Collision from left;
+  					player.speedX = 0;
+  					player.x -= currentOverlap.x;
+  				}
+  			}
+  		}
+    });
 }
 
 Level = function(data){
@@ -231,12 +299,16 @@ function keyDownHandler(e) {
 		break;
 
 		case 87: // w key
-			player.up = true;
+			player.jump = true;
 		break;
 
 		case 83: // s key
 			// TODO: Use this for attack?
 			player.down = true;
+		break;
+
+		case 80: // p key
+			paused = !paused;
 		break;
 
 	}
@@ -253,14 +325,13 @@ function keyUpHandler(e) {
 		break;
 
 		case 87: // w key
-			player.up = false;
+			player.jump = false;
 		break;
 
 		case 83: // s key
 			// TODO: Use this for attack?
 			player.down = false;
 		break;
-
 	}
 }
 
@@ -332,14 +403,10 @@ var leaderBoard = function (){
 
 //Function that opens pause canvas
 var isPaused = function(){
-
-	var canvas = document.getElementById("game");
-    var ctx = document.getElementById("game").getContext("2d");
-
 	// Move draw to the div paused  using ralative
 	ctx.beginPath();
-	ctx.fillStyle= "red";
-	ctx.fillText('GAME PAUSED' ,500,150);
+	ctx.fillStyle = "red";
+	ctx.fillText('GAME PAUSED' ,500, 150);
 	//Add buttons
 
 	ctx.strokeStyle = "blue";
@@ -351,7 +418,7 @@ var isPaused = function(){
 }
 
 function update() {
-	if (paused){
+	if (paused) {
 		//generatePaused();
 		isPaused();
 		return;
@@ -362,10 +429,10 @@ function update() {
 		return;
 	}
 
-	ctx.fillText('SCORE: ' + score,200,50);
+	// Updating the score;
+	ctx.fillText('SCORE: ' + score, 200, 50);
+
 	player.update();
-	player.testCollisions();
-	player.animation.update();
 
 	// TODO: Update all the other entities based
 	// on the speed of the player
@@ -373,6 +440,9 @@ function update() {
 	obstacles.forEach(function(tile) {
 		tile.update();
 	});
+
+	testCollisions();
+	player.animation.update();
 
 	canvasDraw();
 }
