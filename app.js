0a76558e697f6165327ae746a32c56f8df74631e
Sahil Anand <sahil9494@hotmail.com>
2019-04-08 20:05:47 -0230

    remove unused var

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var fs = require ('fs');
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var hPassword;
 
 app.get('/', function(req,res) {
 	res.sendFile(__dirname + '/client/index.html');
