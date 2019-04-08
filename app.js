54b6453f18b757344cc35336bfd6904a7613adde
Sahil Anand <sahil9494@hotmail.com>
2019-04-08 20:04:13 -0230

    store, retreive and update password hash

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var fs = require ('fs');
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var hPassword;
 
 app.get('/', function(req,res) {
 	res.sendFile(__dirname + '/client/index.html');
@@ -100,6 +101,9 @@ io.sockets.on('connection',function(socket) {
 				socket.emit('signUpResponse', {success:false});
 			}
 			else {
+				var passwordHash = require('password-hash');
+				var hashedPassword = passwordHash.generate(data.password);
+				data.hashedPassword = hashedPassword;
 				Database.addUser(data, function() {
 					socket.emit('signUpResponse', {success:true}); 
 				});
