5a2fafb8cc0ed34c62758cd3c699870641061c4d
hparpia8 <hparpia8@gmail.com>
2019-04-14 19:58:12 -0230

    Added new weapon

--- server/DatabaseManager.js
+++ server/DatabaseManager.js
@@ -37,6 +37,7 @@ const assetCollection = [
 							{"type": "Texture", "name": "NewYork", "path": "/client/images/NewYork001.png"},
 							{"type": "Texture", "name": "LosAngeles", "path": "/client/images/LosAngeles.png"},
 							{"type": "Texture", "name": "Bullet", "path": "/client/images/bullets.png"},
+							{"type": "Texture", "name": "Laser", "path": "/client/images/laser.png"},
 							{"type": "Font", "name": "Helvetica", "path": "client/fonts/helvetica.ttf"},
 							{"type": "Sound", "name": "StoryMode", "path": "client/sound/background.mp3"}
 						];
